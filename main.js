07820d6293b9276216062b191393567836ca92d2
Sana Hameed <sanahameed143@gmail.com>
2024-04-03 21:08:28 -0600

    first version

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+   
+    $("#collapse").click(function(){
+        $("#logo").toggle()
+    })
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+
+})
+
+
+
